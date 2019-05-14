29d3d8a84b9fa2eac0cc27f584b895ca8e763e82
minhtu <minhminhtu96@gmail.com>
2019-05-14 20:58:40 +0700

    update conflit

--- Server/server.js
+++ Server/server.js
@@ -31,22 +31,6 @@ app.get('/favicon.ico', function (req, res) {
     res.sendStatus(204);
 });
 
-// express doesn't consider not found 404 as an error so we need to handle 404 explicitly
-// handle 404 error
-// app.use(function (req, res, next) {
-//     let err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-// handle errors
-app.use(function (err, req, res, next) {
-    console.log(err);
-
-    if (err.status === 404)
-        res.status(404).json({ message: "Not found" });
-    else
-        res.status(500).json({ message: "Something looks wrong :( !!!" });
-});
 app.listen(PORT,  ()=>{
     console.log(`Server listening on port ${PORT}`);
 });
