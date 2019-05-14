c6fe0d9f3109b2241fb62833a248382417686e4b
minhtu <minhminhtu96@gmail.com>
2019-05-14 21:03:24 +0700

    update

--- Server/server.js
+++ Server/server.js
@@ -4,28 +4,34 @@ const users = require('./routes/routes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('./config/database'); //database configuration
+
+var jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const app = express();
 app.set('secretKey', 'nodeRestApi'); // jwt secret token
+
+
+//config enviroment
+dotenv.config()
+const PORT = process.env.PORT || 5000
+
 // connection to mongodb
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.json(data);
-});
 
 app.use(cors());
 
-//config enviroment
-dotenv.config()
-const PORT = process.env.PORT || 5000
 // public route
 app.use('/login', users);
 app.use('/api/Studen', users)
+
 // private route
 app.get('/favicon.ico', function (req, res) {
     res.sendStatus(204);
 });
+//run server
 app.listen(PORT,  ()=>{
     console.log(`Server listening on port ${PORT}`);
 });
